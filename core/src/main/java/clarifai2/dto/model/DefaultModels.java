53f3f268e67d7ff551c3d03b4a4ef8f7f857fa71
Kevin Most <kevinmost@users.noreply.github.com>
2016-12-01 14:53:27 -0500

    Refresh default models when the client instance is created (#129)

--- core/src/main/java/clarifai2/dto/model/DefaultModels.java
+++ core/src/main/java/clarifai2/dto/model/DefaultModels.java
@@ -9,36 +9,64 @@ import java.util.concurrent.atomic.AtomicReference;
 
 public final class DefaultModels {
 
-  @NotNull private final ConceptModel general;
-  @NotNull private final ConceptModel food;
-  @NotNull private final ConceptModel travel;
-  @NotNull private final ConceptModel nsfw;
-  @NotNull private final ConceptModel wedding;
-  @NotNull private final ColorModel color;
-  @NotNull private final FaceDetectionModel faceModel;
+  @NotNull private final ExecutorService updater = Executors.newSingleThreadExecutor();
+
+  @NotNull private AtomicReference<ConceptModel> general;
+  @NotNull private AtomicReference<ConceptModel> food;
+  @NotNull private AtomicReference<ConceptModel> travel;
+  @NotNull private AtomicReference<ConceptModel> nsfw;
+  @NotNull private AtomicReference<ConceptModel> wedding;
+  @NotNull private AtomicReference<ColorModel> color;
+  @NotNull private AtomicReference<FaceDetectionModel> faceModel;
 
   public DefaultModels(@NotNull BaseClarifaiClient client) {
-    general = Model._create(ModelType.CONCEPT, client, "aaa03c23b3724a16a56b629203edc62c", "general-v1.3", null);
-    food = Model._create(ModelType.CONCEPT, client, "bd367be194cf45149e75f01d59f77ba7", "food-items-v1.0", null);
-    travel = Model._create(ModelType.CONCEPT, client, "eee28c313d69466f836ab83287a54ed9", "travel-v1.0", null);
-    nsfw = Model._create(ModelType.CONCEPT, client, "e9576d86d2004ed1a38ba0cf39ecb4b1", "nsfw-v1.0", null);
-    wedding = Model._create(ModelType.CONCEPT, client, "c386b7a870114f4a87477c0824499348", "weddings-v1.0", null);
-    color = Model._create(ModelType.COLOR, client, "eeed0b6733a644cea07cf4c60f87ebb7", "color", null);
-    faceModel = Model._create(ModelType.FACE_DETECTION, client, "a403429f2ddf4b49b307e318f00e528b", "face-v1.3", null);
-    // TODO(Kevin): Eventually refresh these models from the API in the background so we have their output info
+    general = create(ModelType.CONCEPT, client, "aaa03c23b3724a16a56b629203edc62c", "general-v1.3");
+    food = create(ModelType.CONCEPT, client, "bd367be194cf45149e75f01d59f77ba7", "food-items-v1.0");
+    travel = create(ModelType.CONCEPT, client, "eee28c313d69466f836ab83287a54ed9", "travel-v1.0");
+    nsfw = create(ModelType.CONCEPT, client, "e9576d86d2004ed1a38ba0cf39ecb4b1", "nsfw-v1.0");
+    wedding = create(ModelType.CONCEPT, client, "c386b7a870114f4a87477c0824499348", "weddings-v1.0");
+    color = create(ModelType.COLOR, client, "eeed0b6733a644cea07cf4c60f87ebb7", "color");
+    faceModel = create(ModelType.FACE_DETECTION, client, "a403429f2ddf4b49b307e318f00e528b", "face-v1.3");
   }
 
-  @NotNull public ConceptModel generalModel() { return general; }
+  @NotNull private <M extends Model<?>> AtomicReference<M> create(
+      @NotNull ModelType type,
+      @NotNull final BaseClarifaiClient client,
+      @NotNull String id,
+      @NotNull String name
+  ) {
+    final AtomicReference<M> model =
+        new AtomicReference<>(Model.<M>_create(type, client, id, name, null));
+    updater.execute(new Runnable() {
+      @Override public void run() {
+        final M updated = update(client, model.get());
+        model.set(updated);
+      }
+    });
+    return model;
+  }
 
-  @NotNull public ConceptModel foodModel() { return food; }
+  @NotNull
+  private <M extends Model<?>> M update(@NotNull final BaseClarifaiClient client, @NotNull final M unupdatedModel) {
+    @SuppressWarnings("unchecked") final M model =
+        (M) client.getModelByID(unupdatedModel.id()).executeSync().getOrNull();
+    if (model == null) {
+      return update(client, unupdatedModel);
+    }
+    return model;
+  }
 
-  @NotNull public ConceptModel travelModel() { return travel; }
+  @NotNull public ConceptModel generalModel() { return general.get(); }
 
-  @NotNull public ConceptModel nsfwModel() { return nsfw; }
+  @NotNull public ConceptModel foodModel() { return food.get(); }
 
-  @NotNull public ConceptModel weddingModel() { return wedding; }
+  @NotNull public ConceptModel travelModel() { return travel.get(); }
 
-  @NotNull public ColorModel colorModel() { return color; }
+  @NotNull public ConceptModel nsfwModel() { return nsfw.get(); }
 
-  @NotNull public FaceDetectionModel faceDetectionModel() { return faceModel; }
+  @NotNull public ConceptModel weddingModel() { return wedding.get(); }
+
+  @NotNull public ColorModel colorModel() { return color.get(); }
+
+  @NotNull public FaceDetectionModel faceDetectionModel() { return faceModel.get(); }
 }
