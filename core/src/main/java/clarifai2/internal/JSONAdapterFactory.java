33a739adc67687c9bdf7ec2fba158e8cdf9434f7
Kevin Most <kevinmost@users.noreply.github.com>
2017-01-10 11:24:24 -0500

    Fix gson delegate exception (#141)
    * Lazily instantiate Gson delegate adapters to avoid exceptions

--- core/src/main/java/clarifai2/internal/JSONAdapterFactory.java
+++ core/src/main/java/clarifai2/internal/JSONAdapterFactory.java
@@ -70,7 +70,7 @@ public abstract class JSONAdapterFactory<T> implements TypeAdapterFactory {
   }
 
   @NotNull private TypeAdapter<T> buildAdapter(@NotNull Gson gson) {
-    return new Adapter<>(gson, typeToken, serializer, deserializer, passthroughAdapter(gson));
+    return new Adapter<>(gson, typeToken, serializer, deserializer, this);
   }
 
   @NotNull private TypeAdapter<T> passthroughAdapter(@NotNull Gson gson) {
@@ -106,30 +106,59 @@ public abstract class JSONAdapterFactory<T> implements TypeAdapterFactory {
 
 
   private static class Adapter<T> extends TypeAdapter<T> {
+
     @NotNull private final Gson gson;
+
+    /**
+     * The type that should be handled by this adapter
+     */
     @NotNull private final TypeToken<T> token;
+
+    /**
+     * The interface that defines how to serialize this type, or {@code null} if this adapter should not handle
+     * serialization of this type.
+     */
     @Nullable private final Serializer<T> serializer;
+
+    /**
+     * The interface that defines how to deserialize this type, or {@code null} if this adapter should not handle
+     * deserialization of this type.
+     */
     @Nullable private final Deserializer<T> deserializer;
-    @NotNull private final TypeAdapter<T> passthroughAdapter;
+
+    /**
+     * used to build a delegate adapter that is stored into {@link #passthroughAdapter}
+     */
+    @NotNull private final TypeAdapterFactory toBePassedThrough;
+    /**
+     * Should not be used directly; only a lazy backing field that should be grabbed by calling {@link #getPassthroughAdapter()}
+     */
+    @Nullable private TypeAdapter<T> passthroughAdapter;
 
     private Adapter(
         @NotNull Gson gson,
         @NotNull TypeToken<T> token,
         @Nullable Serializer<T> serializer,
         @Nullable Deserializer<T> deserializer,
-        @NotNull TypeAdapter<T> passthroughAdapter
+        @NotNull TypeAdapterFactory toBePassedThrough
     ) {
       this.gson = gson;
       this.token = token;
       this.serializer = serializer;
       this.deserializer = deserializer;
-      this.passthroughAdapter = passthroughAdapter;
+      this.toBePassedThrough = toBePassedThrough;
     }
 
+    @NotNull private TypeAdapter<T> getPassthroughAdapter() {
+      if (passthroughAdapter == null) {
+        passthroughAdapter = gson.getDelegateAdapter(toBePassedThrough, token);
+      }
+      return passthroughAdapter;
+    }
 
     @Override public void write(JsonWriter jsonWriter, T value) throws IOException {
       if (serializer == null) {
-        passthroughAdapter.write(jsonWriter, value);
+        getPassthroughAdapter().write(jsonWriter, value);
       } else {
         final JsonElement serialized = serializer.serialize(value, gson);
         Streams.write(serialized, jsonWriter);
@@ -138,7 +167,7 @@ public abstract class JSONAdapterFactory<T> implements TypeAdapterFactory {
 
     @Override public T read(JsonReader jsonReader) throws IOException {
       if (deserializer == null) {
-        return passthroughAdapter.read(jsonReader);
+        return getPassthroughAdapter().read(jsonReader);
       } else {
         final JsonElement json = Streams.parse(jsonReader);
         return deserializer.deserialize(json, token, gson);
