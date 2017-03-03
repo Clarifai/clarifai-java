56b8f36c0964fa7677c216744b1d82d2893d9d16
Doug Szeto <doug-clarifai@users.noreply.github.com>
2017-03-03 14:16:14 -0500

    Revert "Revert "Added the focus/detection/visual model type so dev/staging/prod tests pass""

---
+++ core/src/main/java/clarifai2/dto/model/output_info/VisualOutputInfo.java
@@ -0,0 +1,10 @@
+package clarifai2.dto.model.output_info;
+
+import com.google.auto.value.AutoValue;
+
+@SuppressWarnings("NullableProblems")
+@AutoValue
+public abstract class VisualOutputInfo extends OutputInfo {
+
+  VisualOutputInfo() {} // AutoValue instances only
+}
