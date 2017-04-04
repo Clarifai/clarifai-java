a255e667adafc9e673c5b5b7bc996cf8f385f3a8
Kevin Cronly <kevin.john.cronly@gmail.com>
2017-04-04 15:11:12 -0400

    Apparel model added (#155)

--- tests/src/test/java/clarifai2/test/CommonWorkflowTests.java
+++ tests/src/test/java/clarifai2/test/CommonWorkflowTests.java
@@ -413,6 +413,13 @@ public class CommonWorkflowTests extends BaseClarifaiAPITest {
     );
   }*/
 
+  @Retry
+  @Test public void t21_testApparelModel() {
+    assertSuccess(client.predict(client.getDefaultModels().apparelModel().id())
+        .withInputs(ClarifaiInput.forImage(ClarifaiImage.of("https://samples.clarifai.com/family.jpg")))
+    );
+  }
+
   @Test public void errorsExposedToUser() {
     final ClarifaiResponse<ConceptModel> response = client.getDefaultModels().generalModel().modify()
         .withConcepts(Action.MERGE, Concept.forID("concept2"))
