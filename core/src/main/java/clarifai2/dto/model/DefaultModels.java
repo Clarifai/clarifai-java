39b662576984cb042f5d9fa0668a955963ef0840
Kevin Cronly <kevin@clarifai.com>
2017-05-26 15:41:57 -0400

    Video now supported in all models

--- core/src/main/java/clarifai2/dto/model/DefaultModels.java
+++ core/src/main/java/clarifai2/dto/model/DefaultModels.java
@@ -23,7 +23,12 @@ public final class DefaultModels {
   @NotNull private final AtomicReference<FaceDetectionModel> face;
   @NotNull private final AtomicReference<DemographicsModel> demographics;
   @NotNull private final AtomicReference<EmbeddingModel> generalEmbed;
-  @NotNull private final AtomicReference<VideoModel> video;
+  @NotNull private final AtomicReference<VideoModel> generalVideo;
+  @NotNull private final AtomicReference<VideoModel> foodVideo;
+  @NotNull private final AtomicReference<VideoModel> nsfwVideo;
+  @NotNull private final AtomicReference<VideoModel> travelVideo;
+  @NotNull private final AtomicReference<VideoModel> weddingVideo;
+  @NotNull private final AtomicReference<VideoModel> apparelVideo;
 
   public DefaultModels(@NotNull BaseClarifaiClient client) {
     general = create(ModelType.CONCEPT, client, "aaa03c23b3724a16a56b629203edc62c", "general-v1.3");
@@ -38,7 +43,15 @@ public final class DefaultModels {
     face = create(ModelType.FACE_DETECTION, client, "a403429f2ddf4b49b307e318f00e528b", "face-v1.3");
     demographics = create(ModelType.DEMOGRAPHICS, client, "c0c0ac362b03416da06ab3fa36fb58e3", "age-gender-ethnicity");
     generalEmbed = create(ModelType.EMBEDDING, client, "bbb5f41425b8468d9b7a554ff10f8581", "general-v1.3");
-    video = create(ModelType.VIDEO, client, "aaa03c23b3724a16a56b629203edc62c", "video-v1.3");
+
+    // This is very poor practice, the user should just be able to enter either an image or a video, and the client
+    // should just handle it as needed. However, the client will need to be refactored before this is possible.
+    generalVideo = create(ModelType.VIDEO, client, "aaa03c23b3724a16a56b629203edc62c", "general-v1.3");
+    foodVideo = create(ModelType.VIDEO, client, "bd367be194cf45149e75f01d59f77ba7", "food-items-v1.0");
+    travelVideo = create(ModelType.VIDEO, client, "eee28c313d69466f836ab83287a54ed9", "travel-v1.0");
+    nsfwVideo = create(ModelType.VIDEO, client, "e9576d86d2004ed1a38ba0cf39ecb4b1", "nsfw-v1.0");
+    weddingVideo = create(ModelType.VIDEO, client, "c386b7a870114f4a87477c0824499348", "weddings-v1.0");
+    apparelVideo = create(ModelType.VIDEO, client, "e0be3b9d6a454f0493ac3a30784001ff", "apparel");
   }
 
   @NotNull private <M extends Model<?>> AtomicReference<M> create(
@@ -92,7 +105,27 @@ public final class DefaultModels {
 
   @NotNull public EmbeddingModel generalEmbeddingModel() { return generalEmbed.get(); }
 
-  @NotNull public VideoModel videoModel() {
-    return video.get();
+  @NotNull public VideoModel generalVideoModel() {
+    return generalVideo.get();
+  }
+
+  @NotNull public VideoModel foodVideoModel() {
+    return foodVideo.get();
+  }
+
+  @NotNull public VideoModel travelVideoModel() {
+    return travelVideo.get();
+  }
+
+  @NotNull public VideoModel nsfwVideoModel() {
+    return nsfwVideo.get();
+  }
+
+  @NotNull public VideoModel weddingVideoModel() {
+    return weddingVideo.get();
+  }
+
+  @NotNull public VideoModel apparelVideoModel() {
+    return apparelVideo.get();
   }
 }
