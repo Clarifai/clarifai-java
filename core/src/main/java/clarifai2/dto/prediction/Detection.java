9fd438a1a08076456c554605751af5e2ad98af4b
Rok Povsic <rok.povsic@gmail.com>
2020-02-16 20:31:37 +0100

    Add appearances to Detection

--- core/src/main/java/clarifai2/dto/prediction/Detection.java
+++ core/src/main/java/clarifai2/dto/prediction/Detection.java
@@ -3,6 +3,7 @@ package clarifai2.dto.prediction;
 import clarifai2.internal.grpc.api.ConceptOuterClass;
 import clarifai2.internal.grpc.api.DataOuterClass;
 import clarifai2.dto.input.Crop;
+import clarifai2.internal.grpc.api.FaceOuterClass;
 import com.google.auto.value.AutoValue;
 import org.jetbrains.annotations.NotNull;
 
@@ -19,6 +20,12 @@ public abstract class Detection extends Prediction {
 
   @NotNull public abstract List<Concept> concepts();
 
+  @NotNull public abstract List<Concept> ageAppearances();
+
+  @NotNull public abstract List<Concept> genderAppearances();
+
+  @NotNull public abstract List<Concept> multiculturalAppearances();
+
 
   @NotNull public static Detection deserialize(DataOuterClass.Region detection) {
     Crop crop = Crop.deserialize(detection.getRegionInfo().getBoundingBox());
@@ -28,6 +35,25 @@ public abstract class Detection extends Prediction {
       concepts.add(Concept.deserialize(concept));
     }
 
-    return new AutoValue_Detection(crop, concepts);
+    List<Concept> ageAppearanceConcepts = new ArrayList<>();
+    List<Concept> genderAppearanceConcepts = new ArrayList<>();
+    List<Concept> multiculturalAppearanceConcepts = new ArrayList<>();
+
+    FaceOuterClass.Face face = detection.getData().getFace();
+    if (face != null) {
+      for (ConceptOuterClass.Concept concept : face.getAgeAppearance().getConceptsList()) {
+        ageAppearanceConcepts.add(Concept.deserialize(concept));
+      }
+      for (ConceptOuterClass.Concept concept : face.getGenderAppearance().getConceptsList()) {
+        genderAppearanceConcepts.add(Concept.deserialize(concept));
+      }
+      for (ConceptOuterClass.Concept concept : face.getMulticulturalAppearance().getConceptsList()) {
+        multiculturalAppearanceConcepts.add(Concept.deserialize(concept));
+      }
+    }
+
+    return new AutoValue_Detection(
+        crop, concepts, ageAppearanceConcepts, genderAppearanceConcepts, multiculturalAppearanceConcepts
+    );
   }
 }
